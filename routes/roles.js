31eb422a9f00d4b758dfaa7b55046858b467eb45
00MRX00 <17515560@mail.ru>
2020-12-19 19:14:57 +0300

    roles requests modified

--- routes/roles.js
+++ routes/roles.js
@@ -6,41 +6,47 @@ const table = "roles";
 
 router.get(prefix, async (_, res) => {
     try {
-        const roles = await pool.query(`select * from ${table};`);
+        const roles = await pool.query(`
+        select
+            roles_id, 
+            roles_name as name, 
+            roles_permissions as permissions
+            from ${table};
+        `);
         return res.status(200).json(roles.rows);
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({ message: e.message });
     }
 })
 
-router.post(prefix, async (req,res) => {
+router.post(prefix, async (req, res) => {
     try {
-        const {name, permissions} = req.body;
-        const response = await pool.query(`insert into ${table} (roles_name, roles_permissions) values ($1,$2)`,[name,permissions]);
-        return res.status(200).json(response.rows);
+        const { name, permissions } = req.body;
+        const response = await pool.query(`insert into ${table} (roles_name, roles_permissions) values ($1,$2)`, [name, permissions]);
+        return res.status(200).json({ message: 'Строка добавлена' });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({ message: e.message });
     }
 })
 
-router.put(prefix+"/:id", async (req,res) => {
+router.put(prefix + "/:id", async (req, res) => {
     try {
-        const {id} = req.params;
-        const {roles_name, roles_permissions} = req.body;
-        const response = await pool.query(`update ${table} set (roles_name,roles_permissions) = ($1,$2) where roles_id=$3`,[roles_name,roles_permissions,id]);
-        return res.status(200).json(response.rows);
+        const { id } = req.params;
+        const { name, permissions } = req.body;
+        const response = await pool.query(`update ${table} set (roles_name,roles_permissions) = ($1,$2) where roles_id=$3`, [name, permissions, id]);
+        return res.status(200).json({ message: 'Строка обновлена' });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({ message: e.message });
     }
 })
 
-router.delete(prefix+"/:id", async (req,res) => {
+router.delete(prefix + "/:id", async (req, res) => {
     try {
-        const {id} = req.params;
-        const response = await pool.query(`delete from ${table} where roles_id=$1`,[id]);
-        return res.status(200).json(response.rows);
+        const { id } = req.params;
+        const response = await pool.query(`delete from ${table} where roles_id=$1`, [id]);
+        return res.status(200).json({ message: 'Строка удалена' });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({ message: e.message });
     }
 })
 
