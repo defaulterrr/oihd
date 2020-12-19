14c8dba7a90bf34f7cf66344eb710e047dfbe74a
defaulterrr <thedefaulterrr@gmail.com>
2020-12-19 19:20:07 +0300

    users responses rehauled

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,9 @@ router.get(prefix, async (_, res) => {
         const users = await pool.query(`select * from ${table};`);
         return res.status(200).json(users.rows);
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -17,9 +19,13 @@ router.post(prefix, async (req,res) => {
     try {
         const { name, role } = req.body;
         const response = await pool.query(`insert into ${table} (users_name, users_role) values ($1,$2)`,[name,role]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка добавлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -28,9 +34,13 @@ router.put(prefix+"/:id", async (req,res) => {
         const {id} = req.params;
         const {name, role} = req.body;
         const response = await pool.query(`update ${table} set (users_name,users_role) = ($1,$2) where users_id=$3`,[name,role,id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка обновлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -38,9 +48,13 @@ router.delete(prefix+"/:id", async (req,res) => {
     try {
         const {id} = req.params;
         const response = await pool.query(`delete from ${table} where users_id=$1`,[id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка удалена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
