f182c56c608c284b17eda62b7375bc9567adf771
defaulterrr <thedefaulterrr@gmail.com>
2020-12-19 19:18:50 +0300

    responses rehauled

--- routes/categories.js
+++ routes/categories.js
@@ -9,7 +9,9 @@ router.get(prefix, async (_, res) => {
         const roles = await pool.query(`select * from ${table};`);
         return res.status(200).json(roles.rows);
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -17,9 +19,13 @@ router.post(prefix, async (req,res) => {
     try {
         const {category_name} = req.body;
         const response = await pool.query(`insert into ${table} (category_name) values ($1)`,[category_name]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка добавлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -28,9 +34,13 @@ router.put(prefix+"/:id", async (req,res) => {
         const {id} = req.params;
         const {category_name} = req.body;
         const response = await pool.query(`update ${table} set category_name = $1 where category_id=$2`,[category_name,id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка обновлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -38,9 +48,13 @@ router.delete(prefix+"/:id", async (req,res) => {
     try {
         const {id} = req.params;
         const response = await pool.query(`delete from ${table} where category_id=$1`,[id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка удалена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
--- routes/status.js
+++ routes/status.js
@@ -9,7 +9,9 @@ router.get(prefix, async (_, res) => {
         const roles = await pool.query(`select * from ${table};`);
         return res.status(200).json(roles.rows);
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -17,9 +19,13 @@ router.post(prefix, async (req,res) => {
     try {
         const {status_label} = req.body;
         const response = await pool.query(`insert into ${table} (status_label) values ($1)`,[status_label]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка добавлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -29,9 +35,13 @@ router.put(prefix+"/:id", async (req,res) => {
         const {status_label} = req.body;
         console.log(id, status_label)
         const response = await pool.query(`update ${table} set status_label = $1 where status_id=$2`,[status_label,id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка обновлена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
@@ -39,9 +49,13 @@ router.delete(prefix+"/:id", async (req,res) => {
     try {
         const {id} = req.params;
         const response = await pool.query(`delete from ${table} where status_id=$1`,[id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).json({
+            message: "Строка удалена"
+        });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(500).json({
+            message: e.message
+        });
     }
 })
 
